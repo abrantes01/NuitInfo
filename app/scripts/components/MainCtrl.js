72969acb3db024fd20fb531434d85cd77def896e
abrantes <abrantes.kevin@gmail.com>
2015-12-04 07:19:43 +0100

    ADD best way

--- app/scripts/components/MainCtrl.js
+++ app/scripts/components/MainCtrl.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('sauvetapp')
-.controller('MainCtrl', function ($scope, $sailsSocket) {
+.controller('MainCtrl', function ($scope, $sailsSocket,$state) {
     
     var lat;
     var long;
@@ -38,6 +38,61 @@ angular.module('sauvetapp')
         
         $scope.showed = true;
     }
+    
+    
+    function calcul(ctr,distance){
+  
+        var taux_remplissage = ctr.nbPeople/ctr.capacity;
+        var ratio_med = ctr.nbPeople/ctr.nbDoctors;
+        
+        if(taux_remplissage <= 0.25) return true;
+        if(taux_remplissage <= 0.50 && ratio_med <= 10) return true;
+        if(taux_remplissage <= 0.50 && ratio_med <= 20 && distance <= 40) return true;
+        return false;
+    }
+
+
+
+    function get_best_center(centres,victim) {
+    
+        //Array to store center,distance,bool_accepted
+        var res = [,,];
+        
+        angular.forEach(centres,function(values,key){
+            console.log(values);
+            var dist = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(victim.latitude,victim.longitude), new google.maps.LatLng(values.latitude,values.longitude));
+            res.push([values,dist,calcul(values,dist)]);
+        }) 
+  
+        
+            //Sort array by distance
+        res.sort(function(a,b){
+           return a[1] - b[1];
+        });
+        
+        //Take the nearest first accepted center 
+        for(var l in res){
+            if(l[2]==true) return l[0];
+        }
+        //else take the nearest center
+            return res[0][0];
+    }
+    
+    $scope.bestCenter = function(){
+        console.log("bestCenter");
+        $sailsSocket.get("/center")
+            .then(function(res){
+                var victim = {latitude: lat, longitude: long};     
+                var best = get_best_center(res.data,victim);
+                $state.go("app.itinary",{id : best.id});
+
+                //console.log(res.data);
+            },function(err){
+               console.log(err); 
+            })
+    }
+    
+    
         $sailsSocket.get("/strategy")
             .then(function(data) {
                 console.log(data);
