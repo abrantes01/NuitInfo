b55bbd74df267622bafdc80327376052b455fb5c
Guillaume Nominé <gnomine@ibsciss.com>
2015-12-04 06:03:47 +0100

    UPDATE build

--- dist/scripts/config.acae3e05.js
+++
@@ -1,9 +0,0 @@
-/**
-* Created by bshen on 23/06/15.
-*/
-
-var config = {
-    appUrl: 'https://backend-scoledge.scalingo.io',
-    //appUrl: 'https://backend-scoledge.scalingo.io:443',
-    nbImages: 13
-};
